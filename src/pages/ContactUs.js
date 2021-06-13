b23de68a2f0c9f830f4c7c6845cd375f8b7a0524
sabarikennady <sabareeshwarankennady@gmail.com>
2021-06-13 19:21:46 +0530

    page transition

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 const ContactUs = () => {
-  return <h1>Contact Us</h1>;
+  return (
+    <motion.div
+      exit="exit"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+    >
+      <h1>Contact Us</h1>;
+    </motion.div>
+  );
 };
 
 export default ContactUs;
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 import { MovieState } from "../movieState";
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 const MovieDetail = () => {
   const history = useHistory();
@@ -17,7 +19,12 @@ const MovieDetail = () => {
   return (
     <>
       {movie && (
-        <Details>
+        <Details
+          exit="exit"
+          variants={pageAnimation}
+          initial="hidden"
+          animate="show"
+        >
           <HeadLine>
             <h2>{movie.title}</h2>
             <img src={movie.mainImg} alt="movie" />
@@ -39,7 +46,7 @@ const MovieDetail = () => {
     </>
   );
 };
-const Details = styled.div`
+const Details = styled(motion.div)`
   color: white;
 `;
 
