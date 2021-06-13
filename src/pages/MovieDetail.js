a0d093e3ef9bafa6c0a18479809f739cca6318b1
sabarikennady <sabareeshwarankennady@gmail.com>
2021-06-13 15:40:54 +0530

    moviedetails

---
+++ src/pages/MovieDetail.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import { MovieState } from "../movieState";
+
+const MovieDetail = () => {
+  const history = useHistory();
+  const url = history.location.pathname;
+  const [movies, setMovies] = useState(MovieState);
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const currentMovie = movies.filter((stateMovie) => stateMovie.url === url);
+    setMovie(currentMovie[0]);
+  }, [movies, url]);
+
+  return (
+    <>
+      {movie && (
+        <Details>
+          <HeadLine>
+            <h2>{movie.title}</h2>
+            <img src={movie.mainImg} alt="movie" />
+          </HeadLine>
+        </Details>
+      )}
+    </>
+  );
+};
+const Details = styled.div``;
+
+const HeadLine = styled.div``;
+export default MovieDetail;
