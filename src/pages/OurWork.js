2126e6b21e558afe1e8b1bfb460305081ab9c717
sabarikennady <sabareeshwarankennady@gmail.com>
2021-06-13 11:32:48 +0530

    added our work contents

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,7 +1,59 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import athelete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
 
 const OurWork = () => {
-  return <h1>Our Work</h1>;
+  return (
+    <Work>
+      <Movie>
+        <h2>The Athelete</h2>
+        <div className="Line"></div>
+        <Link>
+          <img src={athelete} alt="athelete-icon" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Racer</h2>
+        <div className="Line"></div>
+        <Link>
+          <img src={theracer} alt="theracer-icon" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>Good Times</h2>
+        <div className="Line"></div>
+        <Link>
+          <img src={goodtimes} alt="goodtimes-icon" />
+        </Link>
+      </Movie>
+    </Work>
+  );
 };
+
+const Work = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  h2 {
+    padding: 1rem 0rem;
+  }
+`;
+
+const Movie = styled.div`
+  padding-bottom: 10rem;
+  .Line {
+    height: 0.5rem;
+    background: #cccccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
 
 export default OurWork;
